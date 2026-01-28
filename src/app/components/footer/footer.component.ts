5f27dfef8bf9e30c37294d2373e3b90fdd0a8889
veriel-dev <vm.ordiales.garcia@gmail.com>
2026-01-28 11:41:14 +0100

    change StudioLab by StudioDev

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -14,23 +14,23 @@ interface FooterLink {
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html'
+  templateUrl: './footer.component.html',
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear();
-  companyName = 'StudioLab';
+  companyName = 'StudioDev';
   envName = environment.envName;
 
   socialLinks: SocialLink[] = [
     { name: 'Twitter', url: 'https://twitter.com', icon: 'twitter' },
     { name: 'LinkedIn', url: 'https://linkedin.com', icon: 'linkedin' },
     { name: 'GitHub', url: 'https://github.com', icon: 'github' },
-    { name: 'Instagram', url: 'https://instagram.com', icon: 'instagram' }
+    { name: 'Instagram', url: 'https://instagram.com', icon: 'instagram' },
   ];
 
   footerLinks: FooterLink[] = [
     { label: 'Politica de privacidad', href: '#' },
     { label: 'Terminos de uso', href: '#' },
-    { label: 'Cookies', href: '#' }
+    { label: 'Cookies', href: '#' },
   ];
 }
